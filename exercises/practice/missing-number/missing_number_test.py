df5e33ac31fb1e229ebe241114d974516499282e
Grant Geyer <GrantG012@yahoo.com>
2021-04-19 15:18:51 -0400

    Add failure messages for test cases

--- exercises/practice/missing-number/missing_number_test.py
+++ exercises/practice/missing-number/missing_number_test.py
@@ -2,21 +2,26 @@ import unittest
 from missing_number import missing_number
 
 class MyTestCase(unittest.TestCase):
+    def list_test(self, N: int, exclude: list):
+        """
+        Excludes the numbers in a list from the numbers in the range 1 to N and check the
+        user's function works for that exclusion.
+        """
+        for ex in exclude:
+            found = missing_number(list(range(1, ex)) + list(range(ex + 1, N + 1)))
+            self.assertEqual(ex, found, f"Missing number {ex} was found in the list. {found} was returned.")
+
     def test50(self):
         """Several tests with numbers in the range of 1 to 50."""
         N = 50
         exclude = [1, 50, 24, 33, 19, 2, 49]
-        for ex in exclude:
-            found = missing_number(list(range(1, ex)) + list(range(ex + 1, N + 1)))
-            self.assertEqual(ex, found)
+        self.list_test(N, exclude)
 
     def test100(self):
         """Several tests with numbers in the range of 1 to 100."""
         N = 100
         exclude = [1, 100, 50, 24, 33, 19, 2, 49, 10, 80, 70]
-        for ex in exclude:
-            found = missing_number(list(range(1, ex)) + list(range(ex + 1, N + 1)))
-            self.assertEqual(ex, found)
+        self.list_test(N, exclude)
 
 if(__name__ == '__main__'):
     unittest.main()
