d9726a87026de24c8932b46a9e8fc0a5654ae4df
Grant Geyer <GrantG012@yahoo.com>
2021-04-19 15:26:21 -0400

    Condense example code to be more succinct

--- exercises/practice/quadratic-roots/quadratic_roots_example.py
+++ exercises/practice/quadratic-roots/quadratic_roots_example.py
@@ -2,9 +2,7 @@ from z3 import *
 
 def roots(a: float, b: float, c: float) -> tuple:
     """Solves for the two roots of the quadratic expression"""
-    x = Real('x')
-    r1 = Const("r1", RealSort())
-    r2 = Const("r2", RealSort())
+    x, r1, r2 = Reals("x r1 r2")
 
     s = Solver()
     s.add(ForAll([x], a * x ** 2 + b * x + c == a * (x - r1) * (x - r2)))
