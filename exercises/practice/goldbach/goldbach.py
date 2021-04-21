6c17b1049528f591a261b24556f255497fa00b42
andjam19 <42663948+andjam19@users.noreply.github.com>
2021-04-21 18:35:02 -0400

    Create goldbach.py

---
+++ exercises/practice/goldbach/goldbach.py
@@ -0,0 +1,8 @@
+from z3 import *
+
+def goldbach(even: int) -> tuple:
+    """Find the two primes that add up to the even number and return them."""
+
+    # TODO: Write your code here.
+
+    return ()
