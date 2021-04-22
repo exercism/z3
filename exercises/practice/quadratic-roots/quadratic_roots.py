3f101dec03c6abf9228b4052cdb65e2f724732c8
andjam19 <42663948+andjam19@users.noreply.github.com>
2021-04-21 20:10:28 -0400

    Create quadratic_roots.py

---
+++ exercises/practice/quadratic-roots/quadratic_roots.py
@@ -0,0 +1,8 @@
+from z3 import *
+
+def roots(a: float, b: float, c: float) -> tuple:
+    """Solves for the two roots of the quadratic expression"""
+
+    # TODO: Write your code here
+
+    return ()
